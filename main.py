4ca3a9cce0e9d1cd4182808548e44f8e64bf927f
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-01-25 10:05:40 -0600

    added files added files with empty functions and inventory items

---
+++ main.py
@@ -0,0 +1,20 @@
+def viewing_opts():
+    pass
+
+def renting():
+    pass
+
+def returning():
+    pass
+
+def replacement():
+    pass
+
+def total_sales():
+    pass
+
+def tests():
+    pass
+
+if __name__ == '__main__':
+   pass
