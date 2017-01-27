32343bf75f5401a6e2aabc7fe87716c30e21f23f
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-01-27 11:11:40 -0600

    Switched to pytest and changed test filename

---
+++ test_rental.py
@@ -0,0 +1,17 @@
+def test_inventory():
+    pass
+
+def test_renting():
+    pass
+
+def test_purchasing():
+    pass
+
+def test_returning():
+    pass
+
+def test_replacement():
+    pass
+
+def test_total_sales():
+    pass
