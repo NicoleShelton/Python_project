8019f712c6c60440e4a299844460583555d37a19
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-01-27 15:22:28 -0600

    set up first function and ran test. (not completely finished)

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 def inventory():
     'Shows the inventory for all the items in stock'
-    return None
+    with open('inventory.txt', 'r') as file:
+        inventory = file.read()
+        return inventory[33:]
+print(inventory())
 
-def renting():
-    'Calculates rental fees and adds to rented.txt'
-    return None
+# def renting(item):
+#     'Calculates rental fees and adds to rented.txt'
+#     return None
 
-def purchasing():
-    'Calculates replacement fee and adds to replacement.txt'
-    return None
+# def purchasing(item):
+#     'Calculates replacement fee and adds to replacement.txt'
+#     return None
 
-def returning():
-    'Subtracts 10% of item and quantity from the total sales'
-    return None
+# def returning(item):
+#     'Subtracts 10% of item and quantity from the total sales'
+#     return None
 
-def total_sales():
-    'Calculates the total sales of all rented items'
-    return None
+# def total_sales(rented):
+#     'Calculates the total sales of all rented items'
+#     return None
 
-def rented():
-    'History of all rented items with dates, quantities, and prices'
-    return None
+# def rented(renting):
+#     'History of all rented items with dates, quantities, and prices'
+#     return None
 
 
-if __name__ == '__main__':
-   options = input('What action would you like to take(Enter "inventory, rented, or total")\n')
+# if __name__ == '__main__':
+#     options = input('What action would you like to take(Enter "inventory, rented, or total")\n')
--- test_rental.py
+++ test_rental.py
@@ -1,12 +1,13 @@
 import main 
 
 def test_inventory():
-    assert main.inventory() == '''Prom dress, 7, 200
-                               Wedding dress, 10, 300
-                               Pageant dress, 6, 150
-                               Cocktail dress, 8, 100
-                               Evening dress, 9, 175 
-                               Casual dress, 5, 120'''
+    assert main.inventory() == '''
+Prom dress, 7, 200
+Wedding dress, 10, 300
+Pageant dress, 6, 150
+Cocktail dress, 8, 100
+Evening dress, 9, 175 
+Casual dress, 5, 120'''
 
 
 def test_renting():
