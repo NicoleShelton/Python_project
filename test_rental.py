477ef0fa6aeb7a5a9479df8358212f5e40a58537
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-02-02 16:21:09 -0600

    added new test

--- test_rental.py
+++ test_rental.py
@@ -1,13 +1,8 @@
 import main 
 
 def test_inventory():
-    assert main.inventory() == '''
-Prom dress, 7, 200
-Wedding dress, 10, 300
-Pageant dress, 6, 150
-Cocktail dress, 8, 100
-Evening dress, 9, 175 
-Casual dress, 5, 120'''
+    assert main.inventory() == [['Prom dress', ' 7', ' 200'], ['Wedding dress', ' 10', ' 300'], ['Pageant dress', ' 6', ' 150'], 
+    ['Cocktail dress', ' 8', ' 100'], ['Evening dress', ' 9', ' 175 '], ['Casual dress', ' 5', ' 120']]
 
 
 def test_renting():
@@ -22,6 +17,10 @@ def test_returning():
     assert main.returning('Pageant dress', 1) == 30.0
     assert main.returning('Casual dress', 3) == 36.0
 
-def test_total_sales(rented):
-    assert main.total_sales('total') == 800.0
-    assert main.total_sales('total') == 500.0
+def test_total_sales_rented():
+    assert main.total_sales_rented() == 800.0
+    assert main.total_sales_rented() == 500.0
+
+def test_total_sales_purchased():
+    assert main.total_sales_purchased() == 800.0
+    assert main.total_sales_purchased() == 500.0
