b47a228fd3333f0d34c73644613b47478ee386c5
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-02-07 15:27:09 -0600

    added '$' to inventory and tests

--- test_rental.py
+++ test_rental.py
@@ -1,16 +1,16 @@
 import main 
 
 def test_inventory():
-    assert main.inventory() == [['Prom dress', '7', '200'], ['Wedding dress', '10', '300'], ['Pageant dress', '6', '150'], 
-    ['Cocktail dress', '8', '100'], ['Evening dress', '9', '175'], ['Casual dress', '5', '120']]
+    assert main.inventory() == [['Prom dress', '7', '$200'], ['Wedding dress', '10', '$300'], ['Pageant dress', '6', '$150'], 
+    ['Cocktail dress', '8', '$100'], ['Evening dress', '9', '$175'], ['Casual dress', '5', '$120']]
 
 def test_view_inventory():
-    assert main.view_inventory() == '''Prom dress, 7, 200
-Wedding dress, 10, 300
-Pageant dress, 6, 150
-Cocktail dress, 8, 100
-Evening dress, 9, 175
-Casual dress, 5, 120'''
+    assert main.view_inventory() == '''Prom dress, 7, $200
+Wedding dress, 10, $300
+Pageant dress, 6, $150
+Cocktail dress, 8, $100
+Evening dress, 9, $175
+Casual dress, 5, $120'''
 
 def test_renting():
     assert main.renting('Prom dress', 1) == (101.07, '$121.40')
