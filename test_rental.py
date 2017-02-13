9aed9cf3f1386f909f682b09d31ab6669776155b
Nicole Shelton <nshelton@basecampcodingacademy.org>
2017-02-13 13:11:09 -0600

    added inventory variable to tests

--- test_rental.py
+++ test_rental.py
@@ -1,7 +1,7 @@
 import main 
+inventory = main.load_inventory()
 
 def test_view_inventory():
-    inventory = main.load_inventory()
     assert main.view_inventory(inventory) == '''Casual dress, 5, 120
 Cocktail dress, 8, 100
 Evening dress, 9, 175
@@ -10,16 +10,16 @@ Prom dress, 7, 200
 Wedding dress, 10, 300'''
 
 def test_renting():
-    assert main.renting('Prom dress', 1) == (101.07, '$121.40')
-    assert main.renting('Evening dress', 1) == (101.07, '$118.72')
+    assert main.renting(inventory, 'Prom dress', 1) == (101.07, '$121.40')
+    assert main.renting(inventory, 'Evening dress', 1) == (101.07, '$118.72')
 
 def test_purchasing():
-    assert main.purchasing('Wedding dress', 1) == '321.00'
-    assert main.purchasing('Cocktail dress', 2) == '214.00'
+    assert main.purchasing(inventory, 'Wedding dress', 1) == '321.00'
+    assert main.purchasing(inventory, 'Cocktail dress', 2) == '214.00'
 
 def test_returning():
-    assert main.returning('Pageant dress', 1) == '$15.00'
-    assert main.returning('Casual dress', 3) == '$36.00'
+    assert main.returning(inventory, 'Pageant dress', 1) == '$15.00'
+    assert main.returning(inventory, 'Casual dress', 3) == '$36.00'
 
 # def test_total_sales_rented():
 #     assert main.total_sales_rented() == '$800.00'
